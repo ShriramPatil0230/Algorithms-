5e7ed0d8c499565b4c877f650c10d1f382bae4f4
ShriramPatil0230 <98881933+ShriramPatil0230@users.noreply.github.com>
2022-05-19 18:51:32 +0530

    Delete 02_addtwonumber.py

--- 02_addtwonumber.py
+++
@@ -1,9 +0,0 @@
-# add  number
-a=int(input("Enter your first number:"))
-b=int(input("Enter your second number:"))
-c=int(input("Enter your third number:"))
-print(a,b,c)
-Sum = a+b+c
-print("Sum =",Sum)
-
-   
